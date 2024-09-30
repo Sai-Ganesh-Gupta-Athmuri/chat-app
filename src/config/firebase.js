b464008f9126a1d864cf47defacf8baf646c35ec
Athmuri V V A Sai Ganesh Gupta <21pa1a5408@vishnu.edu.in>
2024-09-30 18:43:06 +0530

    Profile Updation page completed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAT7wt3i6lr1tfU1B6NT3yQO6s9EOi7rJY",
@@ -14,3 +15,47 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+    try{
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+        const user = res.user;
+        await setDoc(doc(db,"users",user.uid),{
+            id:user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey There, I am using the chat app",
+            lastSeen:Date.now()
+        })
+        await setDoc(doc(db,"chats",user.uid),{
+            chatsData:[]
+        })
+        toast.success("Account Created Successfully");
+    }catch(error){
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email,password) => {
+    try{
+        await signInWithEmailAndPassword(auth,email,password);
+        toast.success("Logged In Successfully");
+    }catch(error){
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = async () => {
+    try{
+        await signOut(auth)
+    }catch(error){
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+export {signup, login, logout, auth, db}
