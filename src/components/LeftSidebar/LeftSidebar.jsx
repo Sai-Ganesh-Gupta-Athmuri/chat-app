b697ecb3af46c7431b098a076a554c627739be40
Athmuri V V A Sai Ganesh Gupta <21pa1a5408@vishnu.edu.in>
2024-09-30 22:22:57 +0530

    LeftSideBar implementation completed

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,62 +1,147 @@
-import React from 'react'
-import './LeftSideBar.css'
-import assets from '../../assets/assets'
-import { useNavigate } from 'react-router-dom'
-import { collection, getDoc, getDocs, query, where } from 'firebase/firestore'
-
+import React, { useContext, useState } from 'react';
+import './LeftSideBar.css';
+import assets from '../../assets/assets';
+import { useNavigate } from 'react-router-dom';
+import { arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import { db } from '../../config/firebase';
+import { AppContext } from '../../context/AppContext';
 
 const LeftSidebar = () => {
+  const navigate = useNavigate();
+  const { userData, chatData, chatUser, setChatUser, messagesId, setMessagesId } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
-    const navigate = useNavigate();
+  const inputHandler = async (e) => {
+    try {
+      const input = e.target.value;
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, 'users');
+        const q = query(userRef, where('username', '==', input.toLowerCase()));
+        const querySnap = await getDocs(q);
 
-    const inputHandler = async(e) => {
-        try {
-            const input = e.target.value;
-            const userRef = collection(db,'users');
-            const q = query(userRef,where("username","==",input.toLowerCase()));
-            const querySnap = await getDocs(q);
-
-            if(!querySnap.empty){
-                console.log(querySnap.docs[0].data());
-            }
-        } catch (error) {
-            console.error(error);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          let userExist = false;
+          if (chatData && Array.isArray(chatData)) {
+            chatData.forEach((user) => {
+              if (user.rId === querySnap.docs[0].data().id) {
+                userExist = true;
+              }
+            });
+          }
+          if (!userExist) {
+            setUser(querySnap.docs[0].data());
+          } else {
+            setUser(null);
+          }
+        } else {
+          setUser(null);
         }
+      } else {
+        setShowSearch(false);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
+
+  const addChat = async () => {
+    const messagesRef = collection(db, 'messages');
+    const chatsRef = collection(db, 'chats');
+    try {
+      const newMessageRef = doc(messagesRef);
+      await setDoc(newMessageRef, {
+        createAt: serverTimestamp(),
+        messages: [],
+      });
+
+      await setDoc(doc(chatsRef, user.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: '',
+          rId: userData.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      }, { merge: true });
+      
+      await setDoc(doc(chatsRef, userData.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: '',
+          rId: user.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      }, { merge: true });
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const setChat = async (item) => {
+    // console.log(item);
+    try {
+        setMessagesId(item.messageId);
+        setChatUser(item);
+        const userChatsRef = doc(db,'chats',userData.id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+        const userChatsData = userChatsSnapshot.data();
+        const chatIndex = userChatsData.chatsData.findIndex((c) => c.messageId === item.messageId);
+        userChatsData.chatsData[chatIndex].messageSeen = true;
+        await updateDoc(userChatsRef,{
+            chatsData:userChatsData.chatsData
+        })
+    } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+    }
+    
+  }
 
   return (
-    <div className='ls'>
-        <div className="ls-top">
-            <div className="ls-nav">
-                <img src={assets.logo} alt="" />
-                <div className="menu">
-                    <img src={assets.menu_icon} alt="" />
-                    <div className="sub-menu">
-                        <p onClick={() => navigate('/profile')}>Edit Profile</p>
-                        <hr />
-                        <p>Logout</p>
-                    </div>
-                </div>
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+            <div className="sub-menu">
+              <p onClick={() => navigate('/profile')}>Edit Profile</p>
+              <hr />
+              <p>Logout</p>
             </div>
-            <div className="ls-search">
-                <img src={assets.search_icon} alt="" />
-                <input onChange={inputHandler} type="text" placeholder='Search here..' />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input onChange={inputHandler} type="text" placeholder="Search here.." />
+        </div>
+      </div>
+      <div className="ls-list">
+        {showSearch && user ? (
+          <div onClick={addChat} className="friends add-user">
+            <img src={user.avatar} alt="" />
+            <p>{user.name}</p>
+          </div>
+        ) : chatData && Array.isArray(chatData) && chatData.length > 0 ? (
+          chatData.map((item, index) => (
+            <div onClick={()=>setChat(item)} key={index} className={`friends ${item.messageSeen || item.messageId === messagesId ? "" : "border" }`}>
+              <img src={item.userData.avatar} alt="" />
+              <div>
+                <p>{item.userData.name}</p>
+                <span>{item.lastMessage}</span>
+              </div>
             </div>
-        </div>
-        <div className="ls-list">        
-            {Array(12).fill("").map((item,index)=>(
-                <div key={index} className="friends">
-                    <img src={assets.profile_img} alt="" />
-                    <div>
-                        <p>SaiRam</p>
-                        <span>Hello, how are you?</span>
-                    </div>
-                </div>
-            ))}
-        </div>
+          ))
+        ) : (
+          <p></p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
